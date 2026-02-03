d1bf5b394f7ba879840967b764a183f584684820
Karan Pratap Singh <priyanshujadoun12345@gmail.com>
2026-02-03 12:33:28 +0530

    Create pattern1.cpp

---
+++ lec3/pattern1.cpp
@@ -0,0 +1,24 @@
+// print ***
+// ***
+// ***
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of rows: ";
+    cin >> n;
+    int i =1;
+    while (i<=n )
+    {
+        int j=1;
+        while(j<=n){
+            cout <<"*";
+            j++; 
+        }
+        cout<< endl;
+        i++;
+    }
+
+    
+
+}
