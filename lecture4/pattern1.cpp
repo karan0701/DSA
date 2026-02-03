ed5182b4c2439d95541e229e379ce84c392fb828
Karan Pratap Singh <priyanshujadoun12345@gmail.com>
2026-02-04 01:09:38 +0530

    .

---
+++ lecture4/pattern1.cpp
@@ -0,0 +1,21 @@
+// print 
+// 1234
+// 1234
+// 1234
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    while (i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<j<<" ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
+
