3dea0565621177648aa6a4ac77435381bab84ae6
Karan Pratap Singh <priyanshujadoun12345@gmail.com>
2026-02-03 23:47:02 +0530

    Update pattern2.cpp

---
+++ lec3/pattern2.cpp
@@ -0,0 +1,25 @@
+// print 111
+// 222
+// 333
+// 444
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value of n: ";
+    cin>>n;
+    int i =1;
+    while (i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<i<<" ";
+            j++;
+
+        }
+        cout<<endl;
+        i++;
+    }
+    
+
+}
