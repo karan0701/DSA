e19c61f329d225fa7146bfd5636e1915f87acfc8
Karan Pratap Singh <priyanshujadoun12345@gmail.com>
2026-02-07 10:09:48 +0530

    Update forloop.cpp

---
+++ lecture5/forloop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    cout<<"Numbers btwn 1 to n is: ";
+    for(int i=1; i<=n; i++){
+        cout<<i<<" ";
+    }
+}
