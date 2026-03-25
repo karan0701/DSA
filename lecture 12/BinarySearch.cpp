260f364684d4973b4865e93563fbc78f686067e8
Karan Pratap Singh <priyanshujadoun12345@gmail.com>
2026-03-26 00:04:21 +0530

    Update BinarySearch.cpp

--- lecture 12/BinarySearch.cpp
+++ lecture 12/BinarySearch.cpp
@@ -6,7 +6,7 @@ int BinarySearch(int arr[], int size, int key){
     int start = 0;
     int end = size-1;
 
-    int mid = (start + end) / 2;
+    int mid = start + ((end - start) / 2);
     while(start <= end){
         if(arr[mid] == key){
             return mid;
@@ -17,13 +17,20 @@ int BinarySearch(int arr[], int size, int key){
         else{
             end = mid - 1;
         }
-        mid = (start + end) / 2;
+        mid = start + ((end - start) / 2);
     }
+    return -1;
 }
 
 int main(){
     int even[6] = {2,4,6,8,10,12};
-    int odd[5] = {1,3,5,7,9};
+    int odd[5] = {1,3,5,7,8};
+
+    int evenIndex = BinarySearch(even, 6, 8);
+    cout<< "index of 8 is " << evenIndex << endl;
+
+    int oddIndex = BinarySearch(odd, 5, 8);
+    cout<< "index of 8 is " << oddIndex << endl;
 
 
     return 0;
