23ff1fd9a3614b06378909600cda6511121d346d
Karan Pratap Singh <priyanshujadoun12345@gmail.com>
2026-06-30 12:29:41 +0530

    .

--- lecture 12/BinarySearch.cpp
+++ lecture 12/BinarySearch.cpp
@@ -11,13 +11,16 @@ int BinarySearch(int arr[], int size, int key){
         if(arr[mid] == key){
             return mid;
         }
+        // go to right wala part agar key badi hai to
         else if(arr[mid] < key){
             start = mid + 1;
         }
+
+    // go to left wala part agar key choti hai to
         else{
             end = mid - 1;
         }
-        mid = start + ((end - start) / 2);
+        mid = start + ((end - start) / 2) ;
     }
     return -1;
 }
@@ -26,11 +29,11 @@ int main(){
     int even[6] = {2,4,6,8,10,12};
     int odd[5] = {1,3,5,7,8};
 
-    int evenIndex = BinarySearch(even, 6, 8);
-    cout<< "index of 8 is " << evenIndex << endl;
+    int evenIndex = BinarySearch(even, 6, 12);
+    cout<< "index of 12 in even array is " << evenIndex << endl;
 
-    int oddIndex = BinarySearch(odd, 5, 8);
-    cout<< "index of 8 is " << oddIndex << endl;
+    int oddIndex = BinarySearch(odd, 5, 7);
+    cout<< "index of 7 in odd array is " << oddIndex << endl;
 
 
     return 0;
--- lecture 13/1-problem.cpp
+++ lecture 13/1-problem.cpp
@@ -1,1 +1,54 @@
-// first and last position of an element in a sorted array 
+// first and last occurances of an element in a sorted array 
+#include <iostream>
+using namespace std;
+int FirstOcc(int arr[], int n, int key){
+    int start = 0;
+    int end = n-1;
+    int ans = -1;
+    int mid = start + (end-start)/2;
+
+    while(start<=end){
+        if(arr[mid] ==key){
+            ans = mid;
+            end = mid-1;
+
+        }
+        else if(key> arr[mid]){ //right mein jaoo
+            start = mid +1;
+
+
+        }
+        else if(key<arr[mid]){ //left mein jaoo
+          end = mid-1;
+
+        }
+        mid = start + (end-start)/2;
+    }
+}
+int LastOcc(int arr[], int n, int key){
+    int start = 0;
+    int end = n-1;
+    int ans = -1;
+    int mid = start + (end-start)/2;
+
+    while(start<=end){
+        if(arr[mid] ==key){
+            ans = mid;
+            start = mid+1;
+
+        }
+        else if(key> arr[mid]){ //right mein jaoo
+            start = mid +1;
+        }
+        else if(key<arr[mid]){ //left mein jaoo
+          end = mid-1;
+        }
+        mid = start + (end-start)/2;
+    }
+}
+int main(){
+    int even[5] = {1,2,3,3,5};
+     cout << "first occurance of 3 is at Index  " << FirstOcc(even,5,3)<< endl;
+     cout << "last occurance of 3 is at Index  " << LastOcc(even,5,3)<< endl;
+    return 0;
+}
